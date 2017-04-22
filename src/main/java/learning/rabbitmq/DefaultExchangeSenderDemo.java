16db7ba440440e7d76ea32fd0beda7dc893c1abd
dapeng <dapeng>
2017-04-22 09:11:14 +0800

    configuration for gradle

---
+++ src/main/java/learning/rabbitmq/DefaultExchangeSenderDemo.java
@@ -0,0 +1,14 @@
+package learning.rabbitmq;
+
+public class DefaultExchangeSenderDemo {
+	public static void sendToDefaultExchange() {
+		Sender sender = new Sender();
+		sender.initialize();
+		sender.send("Test message.");
+		sender.destroy();
+	}
+	
+	public static void main(String[] args) {
+		sendToDefaultExchange();
+	}
+}
